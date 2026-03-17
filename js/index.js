ea13746ae58de8165064e7638d402676f0277562
Viorika Tikhon <viorikatihon301113@gmail.com>
2026-03-17 17:53:31 +0200

    перший коміт

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).scroll(function() {
 if($(document).width() <1024)
     return false;
@@ -25,4 +26,33 @@ $("#hidden-menu .close").on("click", function() {
     $("#hidden-menu").animate({
         "right": "-300px"
     }, 200);
+=======
+$(document).scroll(function() {
+if($(document).width() <1024)
+    return false;
+
+if($(document).scrollTop() > $('.full-page').height() / 2)
+    $("header").addClass("fixed");
+else
+     $("header").removeClass("fixed");
+});
+
+$(".up-btn").on("click", function() {
+ $("html, body").animate({
+    scrollTop: 0
+ }, 'slow');
+});
+
+
+$("#show-menu").on("click", function() {
+    $("#hidden-menu").animate({
+        "right": 0
+    }, 500);
+});
+
+$("#hidden-menu .close").on("click", function() {
+    $("#hidden-menu").animate({
+        "right": "-300px"
+    }, 200);
+>>>>>>> 39e25bf (перший коміт)
 });
